a3a7d5949b0346aed958ead7f571da685829197c
tetsuyaokuyama0312 <y227827@gmail.com>
2021-08-09 19:03:33 +0900

    make exportSchema of Database false

--- app/src/main/java/com/to/markdownnote/repository/MarkdownNoteDatabase.kt
+++ app/src/main/java/com/to/markdownnote/repository/MarkdownNoteDatabase.kt
@@ -4,7 +4,7 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.to.markdownnote.model.Memo
 
-@Database(entities = [Memo::class], version = 1)
+@Database(entities = [Memo::class], version = 1, exportSchema = false)
 abstract class MarkdownNoteDatabase : RoomDatabase() {
     abstract fun memoRepository(): MemoRepository
 }
